b114e9d5ff51aa146d48438a835578b0edc8f76a
courtney <courtne.osborn@ft.com>
2018-01-18 09:15:56 +0000

    Adds paymentFailureAlertBanner manifest  🐿 v2.7.0

--- manifest.js
+++ manifest.js
@@ -6,5 +6,9 @@ module.exports = {
 	licenceJoinPromptSsiClient: {
 		partial: 'bottom/lazy',
 		messageId: 'ssi-join-prompt'
+	},
+	paymentFailureAlertBanner: {
+		partial: 'top/payment-failure',
+		messageId: 'payment-failure-alert-banner'
 	}
 };
