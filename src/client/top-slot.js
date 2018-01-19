9e51549cb683508056cfde58b74e85a37e666058
courtney <courtne.osborn@ft.com>
2018-01-19 16:45:05 +0000

    Updates top-slot generateEvent  🐿 v2.7.0

--- src/client/top-slot.js
+++ src/client/top-slot.js
@@ -6,9 +6,9 @@ const {	messageEvent, listen } = require('./utils');
 const ALERT_BANNER_CLASS = 'n-alert-banner';
 const ALERT_ACTION_SELECTOR = '[data-n-messaging-alert-banner-action]';
 
-module.exports = function ({ config={}, guruResult, customSetup }) {
+module.exports = function ({ config={}, guruResult, customSetup }={}) {
 	let alertBanner;
-	const generateEvent = config.id && messageEvent(config.id);
+	const generateEvent = config.id && messageEvent({ messageId: config.id, position: config.position, variant: config.name, flag: config.flag });
 	const declarativeElement = !config.lazy && config.content;
 	const defaults = { alertBannerClass: ALERT_BANNER_CLASS, autoOpen: false };
 
