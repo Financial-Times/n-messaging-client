abad72c00ea24603242cca1730a2497078281b59
Liam Keaton <liamkeaton@Liams-MacBook.local>
2018-02-02 15:01:42 +0000

    Forgot to add the custom js  🐿 v2.7.0

---
+++ src/components/anon-subscribe-now-teal/main.js
@@ -0,0 +1,16 @@
+module.exports = function customSetup (banner, done) {
+  const actions = banner.innerElement.querySelector('div.n-alert-banner__actions');
+  const button = banner.innerElement.querySelector('a.n-alert-banner__button');
+
+  actions.className += ' n-alert-banner__actions--clickarea';
+  actions.addEventListener('click', event => {
+    // Hit test to see if the click originated on expanded click area
+    if (event.target !== button) {
+      event.stopPropagation();
+      button.click();
+      return false;
+    }
+  });
+
+  done();
+};
