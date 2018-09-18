3b3d1fe355b559774f3bc7b495266d823c0b765a
Jennifer Kerr <jkerr321@gmail.com>
2018-09-18 14:01:53 +0100

    rationalise into one function  🐿 v2.10.3

--- test/smoke.js
+++ test/smoke.js
@@ -6,23 +6,17 @@ const parseSlot = (path) => {
 };
 
 // create array of feature names and slots based on manifest
-const createMessage = function (key) {
-	return {
-		name: key,
-		slot: parseSlot(manifest[key].path)
-	};
-};
-const messages = Object.keys(manifest).map(createMessage);
-
-const messageSlotTests = messages.map(message => {
+const createMessage = (key) => {
+	const slotName = parseSlot(manifest[key].path);
 	return {
 		headers: {
-			'FT-Flags': `${message.slot}:${message.name}`
+			'FT-Flags': `${slotName}:${key}`
 		},
 		urls: {
-			[`/${message.slot}-${message.name}`]: 200 // url is irrelevant mainly to help debug
+			[`/${slotName}-${key}`]: 200 // url is irrelevant mainly to help debug
 		}
 	};
-});
+};
+const messageSlotTests = Object.keys(manifest).map(createMessage);
 
 module.exports = messageSlotTests;
