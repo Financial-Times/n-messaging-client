ce380d4c889d291acd791117a27b047ffe9e3ab5
Jennifer Kerr <jkerr321@gmail.com>
2018-09-18 13:46:50 +0100

    lil refactor  🐿 v2.10.3

--- test/smoke.js
+++ test/smoke.js
@@ -1,21 +1,18 @@
 const manifest = require('../manifest');
 
-let messages = [];
-
 const parseSlot = (path) => {
 	if (path.includes('bottom')) return 'messageSlotBottom';
 	if (path.includes('top')) return 'messageSlotTop';
 };
 
 // create array of feature names and slots based on manifest
-for (let key in manifest) {
-	if (manifest.hasOwnProperty(key)) {
-		messages.push({
-			name: key,
-			slot: parseSlot(manifest[key]['path'])
-		});
-	}
-}
+const createMessage = function (key) {
+	return {
+		name: key,
+		slot: parseSlot(manifest[key].path)
+	};
+};
+const messages = Object.keys(manifest).map(createMessage);
 
 const messageSlotTests = messages.map(message => {
 	return {
