021250cd62434c843eb9b6cca8c520a9f894398e
Jennifer Kerr <jkerr321@gmail.com>
2018-09-18 12:59:10 +0100

    dynamically generate list of messages for testing  🐿 v2.10.3

--- test/smoke.js
+++ test/smoke.js
@@ -1,29 +1,21 @@
-const messages = [
-	{
-		name: 'b2bUpsellBanner',
-		slot: 'messageSlotBottom'
-	},
-	{
-		name: 'appPromotingBanner',
-		slot: 'messageSlotBottom'
-	},
-	{
-		name: 'cookieConsent',
-		slot: 'messageSlotBottom'
-	},
-	{
-		name: 'paymentFailure',
-		slot: 'messageSlotTop'
-	},
-	{
-		name: 'anonSubscribeNow',
-		slot: 'messageSlotTop'
-	},
-	{
-		name: 'swgEntitlementsPrompt',
-		slot: 'messageSlotBottom'
+const manifest = require('../manifest');
+
+let messages = [];
+
+const parseSlot = (path) => {
+	if (path.includes('bottom')) return 'messageSlotBottom';
+	if (path.includes('top')) return 'messageSlotTop';
+};
+
+// create array of feature names and slots based on manifest
+for (let key in manifest) {
+	if (manifest.hasOwnProperty(key)) {
+		messages.push({
+			name: key,
+			slot: parseSlot(manifest[key]['path'])
+		});
 	}
-];
+}
 
 const messageSlotTests = messages.map(message => {
 	return {
