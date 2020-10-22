790fef41d6bfc47f127e1766c9187f67661089bd
Vladimir Aleksiev <vladimir.aleksiev@ft.com>
2020-10-22 18:37:55 +0300

    Update seqcret-squirrel

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -11,13 +11,13 @@ module.exports = {
 			'97212eb0-abb9-11e8-8253-48106866cd8a', // demos/templates/layouts/custom-vanilla.html:29
 			'info@companyx\\.com', // docs/corporate-cancellation-message.md:31|36
 			'499f84e9-d7fb-f90d-ba6a-9cbb65060a44', // manifest.js:24
-			'41218b9e-c8ae-c934-43ad-71b13fcb4465', // manifest.js:86
-			'5a2ecc7c-f349-715e-f2c4-ce657532f71c', // server/templates/components/o-message-notice.html:23
+			'41218b9e-c8ae-c934-43ad-71b13fcb4465', // manifest.js:80
 			'eb62aef2-4bdc-0f31-a2c5-6532b8e17896', // server/templates/partials/bottom/content-message.html:16
 			'2a6a4586-ace0-6465-aebe-aa1ecc543271', // server/templates/partials/bottom/us-election-2020-promo.html:17
 			'5f60b8b4-cbf0-18d7-df41-9caa1171e8c1', // server/templates/partials/top/anon-subscribe-now.html:5|18
 			'a44c9005-2a9c-fe2a-9140-1311ff87f25f', // server/templates/partials/top/print-banner-usa.html:4
-			'79c8c0c5-1686-7f28-3aeb-c14688d59749' // server/templates/partials/top/uk-election.html:4
+			'79c8c0c5-1686-7f28-3aeb-c14688d59749', // server/templates/partials/top/uk-election.html:4
+			'6bc930e8-52c0-8e48-32bf-0a0be69db204' // server/templates/partials/top/us-election-week-discount-33.html:5, server/templates/partials/top/us-election-week-discount-50.html:5
 		]
 	}
 };
