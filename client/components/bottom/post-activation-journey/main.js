e153ddb6b9d76bf04395434ef45e7fbadcf3ed6f
Bolaji <bolaji.adeyemi.consultant@ft.com>
2021-01-27 10:51:28 +0000

    Use the Remove method to close banner to avoid triggering the close event

--- client/components/bottom/post-activation-journey/main.js
+++ client/components/bottom/post-activation-journey/main.js
@@ -1,7 +1,7 @@
 module.exports = (banner, done) => {
 	const closeButtons = Array.from(banner.bannerElement.querySelectorAll('.n-messaging-client-messaging-banner-post-activation-journey__action'));
 	closeButtons.forEach((closeButton) =>
-		closeButton.addEventListener('click', () => banner.close()));
+		closeButton.addEventListener('click', () => typeof banner.bannerElement.remove === 'function' && banner.bannerElement.remove()));
 
 	done();
 };
