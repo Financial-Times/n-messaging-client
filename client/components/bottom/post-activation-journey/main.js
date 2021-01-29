ab032216be09096db5c7a3c5ae3afb4b13ccdb41
Bolaji <bolaji.adeyemi.consultant@ft.com>
2021-01-29 09:04:20 +0000

    Refactor to support IE

--- client/components/bottom/post-activation-journey/main.js
+++ client/components/bottom/post-activation-journey/main.js
@@ -3,9 +3,9 @@ module.exports = (banner, done) => {
 	const closeButtons = Array.from(banner.bannerElement.querySelectorAll('.n-messaging-client-messaging-banner-post-activation-journey__action'));
 	closeButtons.forEach((closeButton) => {
 		closeButton.addEventListener('click', (event) => {
-			const isFeedbackBtn = event.target.name === LEAVE_FEEDBACK && typeof banner.bannerElement.remove === 'function';
+			const isFeedbackBtn = event.target.name === LEAVE_FEEDBACK;
 
-			isFeedbackBtn ? banner.bannerElement.remove() : banner.close();
+			isFeedbackBtn ? banner.bannerElement.parentNode.removeChild(banner.bannerElement) : banner.close();
 		});
 	});
 
