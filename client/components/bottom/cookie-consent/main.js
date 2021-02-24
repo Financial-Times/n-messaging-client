9d7889ff7ef5f241fa29b24ed6fedbd0fc94fd1c
notlee <notlee@users.noreply.github.com>
2021-02-24 10:14:21 +0000

    Restore cookie message tracking.

--- client/components/bottom/cookie-consent/main.js
+++ client/components/bottom/cookie-consent/main.js
@@ -1,5 +1,33 @@
 const oCookieMessage = require('o-cookie-message').default;
 
-module.exports = function customSetup () {
-	oCookieMessage.init();
+const { generateMessageEvent, listen } = require('./utils');
+
+const BOTTOM_SLOT_FLAG = 'messageSlotBottom';
+
+module.exports = function customSetup (config) {
+	const cookieMessage = oCookieMessage.init();
+
+	try {
+		trackCookieMessageInteractions(cookieMessage, config);
+	} catch (error) {
+		console.warn('Could not report cookie message interactions.', error); // eslint-disable-line no-console
+	}
 };
+
+function trackCookieMessageInteractions (cookieMessage, config) {
+	const trackEventAction = generateMessageEvent({
+		flag: BOTTOM_SLOT_FLAG,
+		messageId: config.name,
+		position: config.slot,
+		trackingContext: config.trackingContext || {},
+		variant: config.name
+	});
+
+	listen(cookieMessage.cookieMessageElement, 'oCookieMessage.close', () => trackEventAction('close'));
+	listen(cookieMessage.cookieMessageElement, 'oCookieMessage.view', () => trackEventAction('view'));
+
+	const actionElements = document.querySelectorAll('[data-n-messaging-manage-cookies],[data-n-messaging-accept-cookies]');
+	actionElements.forEach(actionElement => {
+		listen(actionElement, 'click', () => trackEventAction('act', actionElement.textContent));
+	});
+}
