54043efc9b18eb34ead6329581e9e09d2b21537c
Matias Aguero <mjaguero.work@gmail.com>
2021-08-27 13:34:36 +0200

    ACQ-1121: Static messages for Slider changes (Sept Campaign)

---
+++ secret-squirrel.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+	files: {
+		allow: [],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'4312a4ca\\x2daeac\\x2d11e8\\x2d99ca\\x2d68cf89602132', // demos/templates/layouts/custom-vanilla.html:14
+			'97212eb0\\x2dabb9\\x2d11e8\\x2d8253\\x2d48106866cd8a', // demos/templates/layouts/custom-vanilla.html:29
+			'info@companyx\\.com', // docs/corporate-cancellation-message.md:31|36
+			'499f84e9\\x2dd7fb\\x2df90d\\x2dba6a\\x2d9cbb65060a44', // manifest.js:24
+			'41218b9e\\x2dc8ae\\x2dc934\\x2d43ad\\x2d71b13fcb4465', // manifest.js:96
+			'eb62aef2\\x2d4bdc\\x2d0f31\\x2da2c5\\x2d6532b8e17896', // server/templates/partials/bottom/content-message.html:16
+			'b8bd3a0b\\x2d8578\\x2daec6\\x2de7af\\x2de81cf68f7b5a', // server/templates/partials/bottom/marketing-popup-prompt-sept-apac.html:28
+			'd642c68e\\x2d978c\\x2dec30\\x2d5737\\x2d9f9abd97d7a8', // server/templates/partials/bottom/marketing-popup-prompt-sept-row.html:28
+			'5f60b8b4\\x2dcbf0\\x2d18d7\\x2ddf41\\x2d9caa1171e8c1', // server/templates/partials/top/anon-subscribe-now.html:5|18
+			'a44c9005\\x2d2a9c\\x2dfe2a\\x2d9140\\x2d1311ff87f25f' // server/templates/partials/top/print-banner-usa.html:4
+		]
+	}
+};
