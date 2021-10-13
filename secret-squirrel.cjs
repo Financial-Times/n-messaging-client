f5d94f777be15fdd48883153cc50258a0dc70547
Stanimir Dimov <stanimir.dimov@ft.com>
2021-10-13 12:30:30 +0300

    Update secret squirre

--- secret-squirrel.cjs
+++ secret-squirrel.cjs
@@ -13,7 +13,7 @@ module.exports = {
 			'41218b9e\\x2dc8ae\\x2dc934\\x2d43ad\\x2d71b13fcb4465', // manifest.js:76
 			'eb62aef2\\x2d4bdc\\x2d0f31\\x2da2c5\\x2d6532b8e17896', // server/templates/partials/bottom/content-message.html:16
 			'5f60b8b4\\x2dcbf0\\x2d18d7\\x2ddf41\\x2d9caa1171e8c1', // server/templates/partials/top/anon-subscribe-now.html:5|18
-			'11370a43\\x2df8b4\\x2d346f\\x2d6793\\x2d4a4cf734f672', // server/templates/partials/top/digital-marketing-new-world.html:5
+			'16b14c00\\x2dc43e\\x2df3fb\\x2da3fd\\x2d2355127d3e69', // server/templates/partials/top/digital-marketing-new-world.html:5
 			'a44c9005\\x2d2a9c\\x2dfe2a\\x2d9140\\x2d1311ff87f25f' // server/templates/partials/top/print-banner-usa.html:4
 		]
 	}
