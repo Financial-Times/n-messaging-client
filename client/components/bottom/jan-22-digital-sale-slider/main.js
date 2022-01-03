692ca6ff2ba7c7b41ed225c63e92fe512d3af52e
Matias Aguero <mjaguero.work@gmail.com>
2022-01-03 16:48:09 +0100

    ACQ-1374 and ACQ-1375: Top and botton messages for JAN 22 campaign.

---
+++ client/components/bottom/jan-22-digital-sale-slider/main.js
@@ -0,0 +1,20 @@
+const cookies = require('js-cookie');
+
+const ONE_DAY_MS = 24 * 60 * 60 * 1000;
+const POPUP_PROMPT_COOKIE_NAME = 'jan22SalePromptShown';
+
+module.exports = (banner, done) => {
+	const timeSet = cookies.get(POPUP_PROMPT_COOKIE_NAME);
+
+	if (timeSet === undefined) {
+		banner.open();
+	}
+
+	const closeButton = banner.bannerElement.querySelector('.o-banner__close');
+	closeButton.addEventListener('click', function addCookie () {
+		const expiryDate = new Date(new Date().getTime() + 10 * ONE_DAY_MS);
+		cookies.set(POPUP_PROMPT_COOKIE_NAME, 'OK', { domain: 'ft.com', expires: expiryDate });
+	});
+
+	return done({ skip: true });
+};
--- secret-squirrel.cjs
+++ secret-squirrel.cjs
@@ -6,16 +6,17 @@ module.exports = {
 	strings: {
 		deny: [],
 		denyOverrides: [
-			'da448d11-851d-545c-dc0e-e9a2aef62e54', // open-day-premium-upsell.html:13
 			'4312a4ca\\x2daeac\\x2d11e8\\x2d99ca\\x2d68cf89602132', // demos/templates/layouts/custom-vanilla.html:14
 			'97212eb0\\x2dabb9\\x2d11e8\\x2d8253\\x2d48106866cd8a', // demos/templates/layouts/custom-vanilla.html:29
 			'info@companyx\\.com', // docs/corporate-cancellation-message.md:31|36
+			'16b14c00\\x2dc43e\\x2df3fb\\x2da3fd\\x2d2355127d3e69', // docs/lazy-and-static-messages.md:37, server/templates/partials/top/digital-marketing-new-world.html:5
+			'41218b9e\\x2dc8ae\\x2dc934\\x2d43ad\\x2d71b13fcb4465', // docs/lazy-and-static-messages.md:84, manifest.js:73
 			'499f84e9\\x2dd7fb\\x2df90d\\x2dba6a\\x2d9cbb65060a44', // manifest.js:21
-			'41218b9e\\x2dc8ae\\x2dc934\\x2d43ad\\x2d71b13fcb4465', // manifest.js:76
 			'eb62aef2\\x2d4bdc\\x2d0f31\\x2da2c5\\x2d6532b8e17896', // server/templates/partials/bottom/content-message.html:16
+			'19dc305c\\x2dd9dd\\x2d22c6\\x2d6c05\\x2d6dbd76417fd7', // server/templates/partials/bottom/jan-22-digital-sale-slider.html:28
 			'5f60b8b4\\x2dcbf0\\x2d18d7\\x2ddf41\\x2d9caa1171e8c1', // server/templates/partials/top/anon-subscribe-now.html:5|18
 			'0b1f847b\\x2d03f4\\x2d87d5\\x2d1f1d\\x2def958102d37c', // server/templates/partials/top/cop-open-day-climate-capital-anon.html:5
-			'16b14c00\\x2dc43e\\x2df3fb\\x2da3fd\\x2d2355127d3e69', // server/templates/partials/top/digital-marketing-new-world.html:5
+			'54c9ee8b\\x2d08b5\\x2d3a4f\\x2d0dbd\\x2db0d60b121d2e', // server/templates/partials/top/jan-22-digital-sale-banner.html:5
 			'a44c9005\\x2d2a9c\\x2dfe2a\\x2d9140\\x2d1311ff87f25f' // server/templates/partials/top/print-banner-usa.html:4
 		]
 	}
