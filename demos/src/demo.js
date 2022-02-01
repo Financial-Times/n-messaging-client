9d5ead03f83e5bbae0c0d55d5de7cd8bdb2beef5
Asuka.Ochi <asuka.ochi@ft.com>
2022-02-01 13:19:38 +0000

    Remove unavailable function for o-tracking v3

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -5,12 +5,7 @@ document.documentElement.classList.add('js', 'enhanced');
 
 oTracking.init({
 	server: 'https://spoor-api.ft.com/px.gif',
-	context: {
-		product: 'next'
-	},
-	user: {
-		ft_session: oTracking.utils.getValueFromCookie(/FTSession=([^;]+)/)
-	}
+	context: { product: 'next' }
 });
 
 oTracking.click.init('cta');
