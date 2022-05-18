130c89b8a2b52920a4bd40a61967be1a701993d5
James Nicholls <james.nicholls@ft.com>
2022-05-18 14:18:28 +0100

    Implement config, styles and behaviour for Academia sharing message

---
+++ client/components/bottom/academia-cta/main.js
@@ -0,0 +1,66 @@
+const Message = require('@financial-times/o-message').default;
+const pageKitFlags = require('@financial-times/dotcom-ui-flags');
+
+let successMessages = new Map();
+
+const handleActionClickEvent = event => {
+    if (!event.target.classList.contains('o-banner__action')) {
+        return;
+    }
+
+    event.preventDefault();
+
+    const target = event.target;
+    const urlSelectorElement = target.closest('.academia-cta-url-selector');
+    const anchorElement = urlSelectorElement.querySelector('.academia-cta-url-selector__url')
+
+    // as there are two content elements (one for small screens, one for large)
+    // we need to identify which element the click originated from
+    // and generate a unique query selector to pass to o-message
+    const bannerContentElement = target.closest('.o-banner__content');
+    const bannerContentSelector = Array.from(bannerContentElement.classList.values()).reduce((prev, curr) => {
+        return `${prev}.${curr}`;
+    }, '');
+
+    navigator.clipboard.writeText(anchorElement.innerText)
+        .then(() => {
+            urlSelectorElement.style.display = 'none';
+
+            if (successMessages.has(bannerContentSelector)) {
+                successMessages.get(bannerContentSelector).open();
+            } else {
+                const msg = new Message(null, {
+                    type: 'alert',
+                    state: 'success',
+                    content: {
+                        detail: 'The link has been copied to the clipboard',
+                    },
+                    parentElement: `${bannerContentSelector} .academia-cta-message-container`,
+                });
+    
+                msg.messageElement.addEventListener('o.messageClosed', () => {
+                    urlSelectorElement.style.display = 'flex';
+                });
+
+                // store the message in a map for later retrieval
+                // as o-message has no method for deletion/removal
+                successMessages.set(bannerContentSelector, msg);
+            }
+        });
+};
+
+module.exports = (banner, done) => {
+    const flags = pageKitFlags.init();
+    const bannerElement = banner.bannerElement;
+
+    if (flags.get('academicCtaTest') !== 'variant') {
+		return done({ skip: true });
+	}
+
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+        bannerElement.querySelectorAll('.o-banner__action').forEach(buttonElement => buttonElement.classList.remove('n-ui-hide'));
+        bannerElement.addEventListener('click', handleActionClickEvent);
+    }
+
+    done();
+};
