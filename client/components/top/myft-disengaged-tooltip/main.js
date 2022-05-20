e24e229d5c889e675437b10f8386653e37572c30
Rashedul Hassan <rashedul@outlook.com>
2022-05-20 09:50:46 +0100

    Fix path to point to myft-client

--- client/components/top/myft-disengaged-tooltip/main.js
+++ client/components/top/myft-disengaged-tooltip/main.js
@@ -4,7 +4,7 @@
 // - who haven't seen the tooltip more than 3 times
 
 import Tooltip from '@financial-times/o-tooltip';
-import myftClient from 'next-myft-client/myft-npm';
+import myftClient from 'next-myft-client/myft-client';
 import {broadcast} from 'n-ui-foundations';
 import Cookies from 'js-cookie';
 
